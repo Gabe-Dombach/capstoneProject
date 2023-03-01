84eb042258930ec3aca03db8bf0afee4040fbdf7
FinnMcGloshen <famcgloshen211@stevenscollege.edu>
2023-03-01 08:32:10 -0500

    login page

--- view/login.view.php
+++ view/login.view.php
@@ -3,6 +3,15 @@
 
     </head>
     <body>
-        
+        <div>Joshua's Generals</div>
+        <div class="login">
+        <form method="POST">
+            <input type="username" placeholder="username" name="username">
+            <br>
+            <input type="password" placeholder="password" name="password">
+            <br>
+            <input type="submit" name="login">
+        </form>
+        </div>
     </body>
 </html>
