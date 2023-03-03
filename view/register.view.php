726b0b988a1b4c8dea4db50bda41d64a2d4a71c6
FinnMcGloshen <famcgloshen211@stevenscollege.edu>
2023-03-03 10:05:06 -0500

    login and register html & css

--- view/register.view.php
+++ view/register.view.php
@@ -1,8 +1,22 @@
 <html>
     <head>
-
+    <link rel="stylesheet" href="css/register.css" />
     </head>
     <body>
-        
+        <div class="register">
+            <div class="registerform">
+                <div class="title"><h1>Create an account!</h1></div>
+                    <form method="POST">
+                        <input type="username" placeholder="Create Username" name="username" class="input">
+                        <br>
+                        <br>
+                        <input type="password" placeholder="Create Password" name="password" class="input">
+                        <br>
+                        <br>
+                        <input type="submit" name="register" class="submit">
+                    </form>
+                    <p class="link"><a href="login.view.php">Already have an account? Log in here!</p>
+            </div>
+        </div>
     </body>
 </html>
