b648df8ec32c170859dcbea8de81898bf9366bd9
Gabe-Dombach <gdombach009@gmail.com>
2023-03-06 07:49:46 -0500

    made the store page dynamic and added sort by department feature

--- view/store.view.php
+++ view/store.view.php
@@ -1,6 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
     <head>
-
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Joshua's Generals</title>
 
     <?php 
         require "navbar.view.php";
@@ -16,407 +21,59 @@
 
 
     <body>
-        
-    
-    <div class="row">
 
-                    <div class="item">
-
-
-
-                    <img src="../pictures/brake pads.jpg" alt="b pads">
-
-                    <p class="name"> Brake pads</p>
-
-                    <p class="price"> 15.99</p>
-
-                    <p class="dsc">brake pads for cars</p>
-
-
-
-                    </div>
-
-
-                    <div class="item">
-
-
-
-                <img src="../pictures/catalytic converter.jpg" alt="converter">
-
-                <p class="name"> catalytic converter </p>
-
-                <p class="price">12.99</p>
-
-                <p class="dsc"> converter for catalac car</p>
-
-
-
-                    </div>
-
-
-
-
-                    
-                    <div class="item">
-
-
-
-                <img src="../pictures/suspension kit.jpg" alt="suspension">
-
-                <p class="name">suspension kit</p>
-
-                <p class="price">11.99</p>
-
-                <p class="dsc">Full suspension kit</p>
-
-
-
-                    </div>
-
-   
-
-
-    </div>
-
-
-
-
-    <div class="row">
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/acer laptop.jpg" alt="acer laptop">
-
-<p class="name">acer laptop</p>
-
-<p class="price">30.99</p>
-
-<p class="dsc"> 144 hz acer laptop</p>
-
-
-
-    </div>
-
-
-
-    <div class="item">
-
-
-
-                <img src="../pictures/asus monitor.jpg" alt="asus monitor">
-
-                <p class="name">asus monitor</p>
-
-                <p class="price">9.99</p>
-
-                <div class="dsc">244hz  asus monitor</div>
-
-
-
-    </div>
-    
-
-
-
-    <div class="item">
-
-
-
-                <img src="../pictures/bose speaker.jpg" alt="motor oil">
-
-                <p class="name"> bose</p>
-
-                <p class="price">34.99</p>
-
-                <p class="dsc">speaker that whispers lies</p>
-
-
-
-    </div>
-
-
-    </div>
-
-
-    <div class="row">
-
-
-
-    
-
-
-    
-    <div class="item">
-
-
-
-<img src="../pictures/kids jeans.jpg" alt="motor oil">
-
-<p class="name">kids jeans</p>
-
-<p class="price">12.55</p>
-
-<p class="dsc">jeans for children 0 and under</p>
-
-
-
-    </div>
-
-
-    
-    <div class="item">
-
-
-
-<img src="../pictures/kids like a boss shirt.jpg" alt="motor oil">
-
-<p class="name">kids t-shirt</p>
-
-<p class="price">5.99</p>
-
-<p class="dsc">tshirt for the weak willed</p>
-
-
-
-    </div>
-
-
-    
-    <div class="item">
-
-
-
-<img src="../pictures/kids minecraft like a boss shirt.jpg" alt="motor oil">
-
-<p class="name">minecraft kids like a boss tshirt</p>
-
-<p class="price">12.67</p>
-
-<p class="dsc">T shirt for the mentally derranged</p>
-
-
-
-    </div>
-
-
-
-
-    <div class="row">
-
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/camping chair.jpg" alt="motor oil">
-
-<p class="name">Campaing chair</p>
-
-<p class="price">12.99</p>
-
-<p class="dsc">camping chair</p>
-
-
-
-    </div>
-
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/camping cooking set.jpg" alt="motor oil">
-
-<p class="name">camping cooking set</p>
-
-<p class="price">349.99</p>
-
-<p class="dsc">stainless steel camp cooking set</p>
-
-
-
-    </div>
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/camping lanterns.jpg" alt="motor oil">
-
-<p class="name"> Camping lanterns</p>
-
-<p class="price">12.99</p>
-
-<p class="dsc">long lasting camping lanterns</p>
-
-
-
-    </div>
-
-
-
-    </div>
-
-
-    <div class="row">
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/3 seat couch.jpg" alt="motor oil">
-
-<p class="name">3 seat couch</p>
-
-<p class="price"> 20.99 </p>
-
-<p class="dsc"> unsed 3 seat couch </p>
-
-
-
-    </div>
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/dining room table set.jpg" alt="motor oil">
-
-<p class="name">dining room table</p>
-
-<p class="price"> 50.55 </p>
-
-<p class="dsc"> pre owned dining room table</p>
-
-
-
-    </div>
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/dresser.jpg" alt="motor oil">
-
-<p class="name"> dresser </p>
-
-<p class="price"> 69.99</div>
-
-<div class="dsc"> oak wood dresser </div>
-
-
-
-    </div>
-
-    </div>
-
-
-
-    <div row = "row">
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/candy land.jpg" alt="motor oil">
-
-<p class="name">candy land</p>
-
-<p class="price"> 28.99</p>
-
-<p class="dsc"> candyland board game</p>
-
-
-
-    </div>
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/connect 4.jpg" alt="motor oil">
-
-<p class="name"> connect 4</p>
-
-<p class="price"> 15.99</p>
-
-<p class="dsc"> connect 4 board game </p>
-
-
-
-    </div>
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/kids lego set.jpg" alt="motor oil">
-
-<p class="name">lego set</p>
-
-<p class="price">39.99</p>
-
-<p class="dsc"> lego set for 5 and under</p>
-
-
-
-    </div>
-
-    </div>
-
-    <div class="row">
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/elf makeup.jpg" alt="motor oil">
-
-<p class="name"> elf makeup </p>
-
-<p class="price">39.99</p>
-
-<p class="dsc"> elf make up kit </p>
-
-
-
-    </div>
-
-
-
-    <div class="item">
-
-
-
-<img src="../pictures/colgate toothpaste.jpg" alt="motor oil">
-
-<p class="name">colgate toothpaste</p>
-
-<p class="price"> 2.99 </p>
-
-<p class="dsc"> toothpaste</p>
-
-
-
-    </div>
-
-
-    </div>
-   
-
+        <form method="get">
+            <label for="department">Browse by department:</label>
+            <select name="department" id="department">
+                <option value="" <?php if (!isset($_GET['department'])) echo 'selected'; ?>>All departments</option>
+                <option value="automotive_parts" <?php if (isset($_GET['department']) && $_GET['department'] === 'automotive_parts') echo 'selected'; ?>>Automotive parts</option>
+                <option value="electronics" <?php if (isset($_GET['department']) && $_GET['department'] === 'electronics') echo 'selected'; ?>>Electronics</option>
+                <option value="clothing" <?php if (isset($_GET['department']) && $_GET['department'] === 'clothing') echo 'selected'; ?>>Clothing</option>
+                <option value="outdoor_supplies" <?php if (isset($_GET['department']) && $_GET['department'] === 'outdoor_supplies') echo 'selected'; ?>>Outdoor supplies</option>
+                <option value="furniture" <?php if (isset($_GET['department']) && $_GET['department'] === 'furniture') echo 'selected'; ?>>Furniture</option>
+                <option value="childrens_toys_and_games" <?php if (isset($_GET['department']) && $_GET['department'] === 'childrens_toys_and_games') echo 'selected'; ?>>Children’s toys and games</option>
+                <option value="health_and_beauty" <?php if (isset($_GET['department']) && $_GET['department'] === 'health_and_beauty') echo 'selected'; ?>>Health and beauty</option>
+                <option value="uncategorized" <?php if (isset($_GET['department']) && $_GET['department'] === 'uncategorized') echo 'selected'; ?>>Uncategorized</option>             
+            </select>
+            <input type="submit" value="Filter">
+        </form>
+        <?php
+// Build the SQL query to retrieve the inventory items
+$sql = 'SELECT * FROM inventory';
+
+// If a department was selected, modify the SQL query to filter by department
+if (isset($_GET['department']) && $_GET['department'] !== '') {
+    $department = $_GET['department'];
+    $sql .= " WHERE department = '$department'";
+}
+
+// Execute the SQL statement
+$conn = connect();
+
+$res = mysqli_query($conn, $sql);
+
+// Display the inventory items
+$count = 0;
+while ($row = $res->fetch_assoc()) {
+    if ($count % 3 == 0) {
+        echo '<div class="row">';
+    }
+    echo '<div class="item">';
+    echo '<h2>' . htmlspecialchars($row['title']) . '</h2>';
+    echo '<p>' . htmlspecialchars($row['description']) . '</p>';
+    echo '<img src="../pictures/' . htmlspecialchars($row['image']) . '" alt="' . htmlspecialchars($row['imageAlt']) . '">';
+    echo '<p>Price: $' . htmlspecialchars($row['price']) . '</p>';
+    echo '</div>';
+    $count++;
+    if ($count % 3 == 0) {
+        echo '</div>';
+    }
+}
+if ($count % 3 != 0) {
+    echo '</div>';
+}
+
+?>
 
     </body>
 </html>
