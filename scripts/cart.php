74739804201a0e0011e367f35eb7db314a80e6d9
Gabe-Dombach <gdombach009@gmail.com>
2023-03-10 09:01:09 -0500

    added the basics to the carts backend

---
+++ scripts/cart.php
@@ -0,0 +1,26 @@
+<?php 
+
+session_start();
+if(!isset($_SESSION['ID'])){
+    session_destroy();
+    header("Location:login.php?error=Please Login to acess cart");
+}
+require "database.php";
+$id = $_SESSION['ID'];
+$sql = "SELECT * FROM carts WHERE custID = $id";
+$conn = connect();
+
+$res = mysqli_query($conn, $sql);
+
+if(!$res){
+    echo mysqli_error($conn);
+    exit();
+}
+
+if(isset($_POST['removeCartSubmit'])){
+    $itemID = $_POST['removeItemID'];
+    $sql = "DELETE FROM carts WHERE custID = $id AND itemID = $itemID";
+
+}
+
+?>
