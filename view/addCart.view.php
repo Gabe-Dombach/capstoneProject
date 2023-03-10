86670dcc9044e431435065e3e03b02f97a5167c5
Gabe-Dombach <gdombach009@gmail.com>
2023-03-10 08:44:39 -0500

    added AJAX to store page and applied styling to the individual custome page for items

--- view/addCart.view.php
+++ view/addCart.view.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../view/css/addCart.css">
     <title>Document</title>
 </head>
 <body>
@@ -12,7 +13,7 @@
     echo '<div class="item">';
     echo '<h2>' . htmlspecialchars($row['title']) . '</h2>';
     echo '<p>' . htmlspecialchars($row['description']) . '</p>';
-    echo '<img src="../pictures/' . htmlspecialchars($row['image']) . '" alt="' . htmlspecialchars($row['imageAlt']) . '">';
+    echo '<img class="img" src="../pictures/' . htmlspecialchars($row['image']) . '" alt="' . htmlspecialchars($row['imageAlt']) . '">';
     echo '<p>Price: $' . htmlspecialchars($row['price']) . '</p>';
     echo '  <form action=addCart.php method=POST>
                 <input type=hidden name="valueAddCart" value="' . htmlspecialchars($row['ID']).'">
