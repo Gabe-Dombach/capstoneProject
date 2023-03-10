f9f857266b73eddbfbd57c735aebc0f9f73bf3fb
FinnMcGloshen <famcgloshen211@stevenscollege.edu>
2023-03-10 09:37:24 -0500

    customer service and other css

--- view/customersupportpage.view.php
+++ view/customersupportpage.view.php
@@ -1,8 +1,28 @@
 <html>
     <head>
-
+    <link rel="stylesheet" href="css/customerService.css" />
     </head>
     <body>
-        
+        <div class="cs">
+            <div class="csform">
+                <div class="title"><h1>Joshua's Generals Customer Service</h1></div>
+                    <form method="POST">
+                        <input type="text" placeholder="Name" name="name" class="input">
+                        <br>
+                        <br>
+                        <input type="number" placeholder="Phone Number" name="phonenumber" class="input">
+                        <br>
+                        <br>
+                        <input type="text" placeholder="Email" name="email" class="input">
+                        <br>
+                        <br>
+                        <input type="text" class="input" placeholder="Write your message here" id="message">
+                        <br>
+                        <br>
+                        <input type="submit" name="submit" class="submit">
+                    </form>
+                    <p><a href="register.view.php">Click here to create account!</p>
+            </div>
+        </div>
     </body>
 </html>
