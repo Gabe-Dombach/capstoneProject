f933fa6682ed2693d1c8e2fbc2b84fe2f1dd7cbd
FinnMcGloshen <famcgloshen211@stevenscollege.edu>
2023-03-29 09:15:41 -0400

    fixed rating

--- view/addCart.view.php
+++ view/addCart.view.php
@@ -55,11 +55,11 @@
                     <fieldset class="rating">
                         <legend>Please rate:</legend>
 
-                        <input type="radio" id="star1" name="reveiwRating" value="1" /><label for="star1" title="1 star"></label>
-                        <input type="radio" id="star2" name="reveiwRating" value="2" /><label for="star2" title="2 stars"></label>
-                        <input type="radio" id="star3" name="reveiwRating" value="3" /><label for="star3" title="3 stars"></label>
+                        <input type="radio" id="star5" name="reveiwRating" value="5" /><label for="star5" title="5 star"></label>
                         <input type="radio" id="star4" name="reveiwRating" value="4" /><label for="star4" title="4 stars"></label>
-                        <input type="radio" id="star5" name="reveiwRating" value="5" /><label for="star5" title="5 stars"></label>
+                        <input type="radio" id="star3" name="reveiwRating" value="3" /><label for="star3" title="3 stars"></label>
+                        <input type="radio" id="star2" name="reveiwRating" value="2" /><label for="star2" title="2 stars"></label>
+                        <input type="radio" id="star1" name="reveiwRating" value="1" /><label for="star1" title="1 star"></label>
 
 
                     </fieldset>
