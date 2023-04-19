41356f5026537dcc6fd034989461646faca2ddb9
Gabriel Dombach <gabrieldombach@gmail.com>
2023-04-19 08:18:36 -0400

    added account stuff

--- scripts/account.php
+++ scripts/account.php
@@ -42,66 +42,57 @@ require_once "../view/account.view.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // handle card Removal Request
-    if(isset($_POST["cardRemoval"])){
-        // echo $_POST["csrf_token"]."<br/>SESSION:".$_SESSION["csrf_token"]; //testing to make sure validation is working
-        // Verify CSRF token
-        // if (!isset($_POST["csrf_token"]) || $_POST["csrf_token"] !== $_SESSION["csrf_token"]) {
-        //     // CSRF token verification failed - handle the error
-        //     die("verification failed");
-        // }
-
-        // Loop through all submitted card numbers
-        foreach ($_POST['cards'] as $card_num) {
-            // Check if the card should be deleted
-            if (in_array($card_num, $_POST['delete'])) {
-                // Remove the card from the database using a prepared statement
-                $sql = "DELETE FROM cards WHERE crdNum = ?";
-                $stmt = $conn->prepare($sql);
-                $stmt->bind_param("s", $card_num);
-                $stmt->execute();
-                $stmt->close();
-                unset($_SESSION["csrf_token"]);
-                // echo "<script>location.reload();</script>";
-                // exit();
-            }
-            }
+if (isset($_POST["cardRemoval"])) {
+    // Loop through all submitted card numbers
+    foreach ($_POST['cards'] as $card_num) {
+        // Check if the card should be deleted
+        if (in_array($card_num, $_POST['delete'])) {
+            // Remove the card from the database using a prepared statement
+            $sql = "DELETE FROM cards WHERE crdNum = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("s", $card_num);
+            $stmt->execute();
+            $stmt->close();
+            // echo "<script>location.reload();</script>";
+            // exit();
+        }
     }
+
+}
+
 
     // handle password change request
-    if(isset($_POST["changePassword"])){
-        $sql = "SELECT pswrd FROM users WHERE ID = $id";
-        $conn = connect();
-        $res = mysqli_query($conn, $sql);
-        $data = mysqli_fetch_assoc($res);
-        $pass = $data["pswrd"];
-        if(!password_verify($_POST["currPass"], $pass)){
-            die("Incorrect password"); //verify that the user entered their password correctly
-        }
-
-        if($_POST["newPass"] !== $_POST["verifyPass"]) {
-            die("New passwords does not match"); //verify that new password matches the verification input
-        }
-        if(!preg_match("/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/",$_POST["newPass"])){
-            die("Password must be at least 8 characters and contain letters, numbers, and at least one special symbol (@$!%*#?&).");
-        }
-        $newHashPass = password_hash($_POST["newPass"],PASSWORD_DEFAULT);
-        $stmt = $conn->prepare("UPDATE users SET pswrd = (?) WHERE ID = $id");
-        $stmt->bind_param("s",$newHashPass);
-        if($stmt->execute()){
-            echo "Password changed successfully";
-            mysqli_close($conn);
-            exit();
-        }
-
-        else {
-            echo "Error: " . $stmt->error;
-            mysqli_close($conn);
-
-        }
-
-
-
+if(isset($_POST["changePassword"])){
+    $sql = "SELECT pswrd FROM users WHERE ID = ?";
+    $conn = connect();
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $res = $stmt->get_result();
+    $data = $res->fetch_assoc();
+    $pass = $data["pswrd"];
+    if(!password_verify($_POST["currPass"], $pass)){
+        die("Incorrect password"); //verify that the user entered their password correctly
     }
+
+    if($_POST["newPass"] !== $_POST["verifyPass"]) {
+        die("New passwords does not match"); //verify that new password matches the verification input
+    }
+    if(!preg_match("/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/",$_POST["newPass"])){
+        die("Password must be at least 8 characters and contain letters, numbers, and at least one special symbol (@$!%*#?&).");
+    }
+    $newHashPass = password_hash($_POST["newPass"],PASSWORD_DEFAULT);
+    $stmt = $conn->prepare("UPDATE users SET pswrd = ? WHERE ID = ?");
+    $stmt->bind_param("si",$newHashPass, $id);
+    if($stmt->execute()){
+        echo "Password changed successfully";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+    mysqli_close($conn);
+    exit();
+}
+
         //handle card addition request
     if(isset($_POST["newCard"])){
 
@@ -127,7 +118,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $stmt->bind_param("ssss", $id,$crdNum,$secCode,$d);
         $stmt->execute();
         $stmt->close();
-        echo "insertion sucessfull";
+        // echo "insertion sucessfull";
 
 
     }
@@ -141,6 +132,8 @@ $conn->close();
 
 
 
+header("Refresh:0",false);
+exit();
 
 }
 
