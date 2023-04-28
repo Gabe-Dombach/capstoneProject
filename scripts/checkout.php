8f6d40323a74712f5e8ff765bf8c265a3e2ec8a9
malikShort <93655412+malikShort@users.noreply.github.com>
2023-04-28 09:04:13 -0400

    Add files via upload

---
+++ scripts/checkout.php
@@ -0,0 +1,35 @@
+<?php 
+require "database.php";
+session_start();
+if(!isset($_SESSION['ID'])){
+    session_destroy();
+    header("Location:login.php?error=Please Login to access cart");
+}
+
+
+
+$id = $_SESSION['ID'];
+$sql = "SELECT * FROM carts WHERE custID = $id";
+$conn = connect();
+
+$res = mysqli_query($conn, $sql);
+
+if(!$res){
+    echo mysqli_error($conn);
+    exit();
+}
+
+if(isset($_POST['removeCartSubmit'])){
+    $itemID = $_POST['removeItemID'];
+    $sql = "DELETE FROM carts WHERE custID = $id AND itemID = $itemID";
+
+}
+
+
+
+
+require "../view/checkout.view.php";
+
+
+?>
+
