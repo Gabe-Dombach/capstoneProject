fcb5593af80e3222d84538b7fd7c2e75957a2343
FinnMcGloshen <famcgloshen211@stevenscollege.edu>
2023-05-03 10:54:58 -0400

    cart and checkout updates

--- view/cart.view.php
+++ view/cart.view.php
@@ -4,6 +4,7 @@
 <link rel="icon" type="image/png" sizes="32x32" href="../pictures/favicon_io/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="../pictures/favicon_io/favicon-16x16.png">
 <link rel="manifest" href="../pictures/favicon_io/site.webmanifest">
+<link rel="stylesheet" href="../view/css/cart.css">
     </head>
     <body>
         <header><?php require "navbar.view.php";?></header>
